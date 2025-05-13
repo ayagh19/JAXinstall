395f679d7e593ee883129e4422a18b2832206352
Aya Ghaleb <124587945+ayagh19@users.noreply.github.com>
2025-05-13 14:46:46 +0100

    added jax

--- script.sh
+++ script.sh
@@ -4,3 +4,15 @@ module purge
 module load rhel9/default-dawn
 
 module load intelpython-conda
+
+python3 -m venv --system-site-packages venv_project
+
+source venv_project/bin/activate
+
+pip install -U pip
+
+pip install intel-extension-for-openxla
+
+pip install -r https://raw.githubusercontent.com/intel/intel-extension-for-openxla/main/test/requirements.txt
+
+
